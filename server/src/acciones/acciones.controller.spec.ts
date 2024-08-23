9fa944cba265a0e7bddf292d70b5ce2d60f12032
Sergio Pola <spola.cl@gmail.com>
2024-08-23 18:06:36 -0400

    Test que ejecuta

--- server/src/acciones/acciones.controller.spec.ts
+++ server/src/acciones/acciones.controller.spec.ts
@@ -1,6 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AccionesController } from './acciones.controller';
 import { AccionesService } from './acciones.service';
+import { EventEmitterModule } from '@nestjs/event-emitter';
 
 describe('AccionesController', () => {
   let controller: AccionesController;
@@ -9,6 +10,9 @@ describe('AccionesController', () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AccionesController],
       providers: [AccionesService],
+      imports: [
+        EventEmitterModule.forRoot(),
+      ]
     }).compile();
 
     controller = module.get<AccionesController>(AccionesController);
--- server/src/acciones/acciones.service.spec.ts
+++ server/src/acciones/acciones.service.spec.ts
@@ -1,12 +1,18 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AccionesService } from './acciones.service';
+import { EventEmitterModule } from '@nestjs/event-emitter';
 
 describe('AccionesService', () => {
   let service: AccionesService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AccionesService],
+      providers: [
+        AccionesService,
+      ],
+      imports: [
+        EventEmitterModule.forRoot(),
+      ]
     }).compile();
 
     service = module.get<AccionesService>(AccionesService);
@@ -15,4 +21,10 @@ describe('AccionesService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  it('should return all task', () => {
+    let tareas = service.getAll();
+    expect(tareas).toBeDefined();
+    expect(tareas).toHaveLength(0);
+  })
 });
